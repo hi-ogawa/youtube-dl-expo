dbad9b841df1ae0a1d0fd4b2bdc20fc51dc6d4ef
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-15 19:20:14 +0900

    chore: fix prettier

--- packages/client/src/ipc.ts
+++ packages/client/src/ipc.ts
@@ -1,4 +1,6 @@
 import { IpcClient } from "@-/common";
 
-export const ipcClient = new IpcClient(ReactNativeWebView.postMessage.bind(ReactNativeWebView));
+export const ipcClient = new IpcClient(
+  ReactNativeWebView.postMessage.bind(ReactNativeWebView)
+);
 ipcClient.expose(window);
