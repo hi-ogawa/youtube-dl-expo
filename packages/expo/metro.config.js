675cea3e7f826df5fa97eb2b00ebbd37c7cde8f0
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-15 19:22:00 +0900

    chore: remove custom metro config
    - not needed now that common is in-tree

--- packages/expo/metro.config.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-config.watchFolders.push(path.resolve(__dirname, "packages", "common"));
-
-module.exports = config;
